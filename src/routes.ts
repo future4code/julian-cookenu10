c3a1b0ff647af07b1585588cfefd719e47166218
José Mauro Neto <jmauroneto@gmail.com>
2020-07-15 21:38:32 -0300

    endpoints de criar e pegar receita

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {signup}  from './endpoints/signup'; 
+import { createRecipe, getRecipe } from './endpoints/recipe';
 
 const routes = Router();
 
@@ -7,8 +8,8 @@ routes.post('/signup', signup);
 routes.post('/login',);
 routes.get('/user/profile',);
 routes.get('/user/:id',);
-routes.post('/recipe',);
-routes.get('/recipe/:id',);
+routes.post('/recipe', createRecipe);
+routes.get('/recipe/:id', getRecipe);
 routes.post('/user/follow',);
 routes.post('/user/unfollow',);
 routes.get('/user/feed',);
